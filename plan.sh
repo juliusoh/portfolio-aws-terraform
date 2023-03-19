411346459fa4e6cb772afb9843e44c0d5bd3cd7b
Julius Oh <juliusoh@gmail.com>
2023-03-18 20:12:26 -0700

    plan

--- plan.sh
+++ plan.sh
@@ -1,9 +1,3 @@
-set -e
-
-# Authenticate to AWS using the IAM role
-aws configure set role_arn "$AWS_ROLE_ARN"
-aws configure set source_profile default
-
 # Initialize the Terraform backend
 terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="key=$TF_STATE_KEY"
 

3c3030a25b9e164ab5a7e9b29380f3c89de753ac
Julius Oh <juliusoh@gmail.com>
2023-03-19 00:20:11 -0700

    eks module

---
+++ eks.tf
@@ -0,0 +1,9 @@
+module "eks-ignite" {
+  source = "./tf_aws_eks_ignite"
+
+  instance_types = ["t3.medium"]
+  region         = var.region
+  stack_name     = var.stack_name
+  subnet_ids     = module.vpc.eks_subnets
+  account_id     = data.aws_caller_identity.current.account_id
+}

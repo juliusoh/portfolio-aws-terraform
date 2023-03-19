125f8ef089ce2b20e10daf986b50b6aa39cff05b
Julius Oh <juliusoh@gmail.com>
2023-03-18 19:57:02 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,14 +5,4 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::939919218449:role/julius_role"
   }
-}set -e
-
-# Authenticate to AWS using the IAM role
-aws configure set role_arn "$AWS_ROLE_ARN"
-aws configure set source_profile default
-
-# Initialize the Terraform backend
-terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="key=$TF_STATE_KEY"
-
-# Run a Terraform plan and store the output in a file
-terraform plan -out=tfplan.out
+}

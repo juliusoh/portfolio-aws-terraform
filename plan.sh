093c3b9e1fd16ca89af8cd0f2cfba123290ded23
Julius Oh <juliusoh@gmail.com>
2023-03-19 14:43:45 -0700

    terraform dynamo state

--- plan.sh
+++ plan.sh
@@ -8,7 +8,7 @@ echo
 echo "##############################################################################"
 echo
 # Initialize the Terraform backend
-terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="key=$TF_STATE_KEY"
+terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="key=$TF_STATE_KEY" -backend-config="region=$TF_STATE_REGION" -backend-config="dynamodb_table=$TF_STATE_DYNAMODB_TABLE"
 
 # Run a Terraform plan and store the output in a file
 terraform plan -out=plan.out

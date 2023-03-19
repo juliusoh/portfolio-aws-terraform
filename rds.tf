d0a1ae2edda96fa7047689ab8f54718ae2b21876
Julius Oh <juliusoh@gmail.com>
2023-03-19 16:12:02 -0700

    aws rds

--- rds.tf
+++ rds.tf
@@ -1,22 +1,22 @@
-module "rds" {
-  source = "./tf_aws_rds"
-  count  = 1
+# module "rds" {
+#   source = "./tf_aws_rds"
+#   count  = 1
 
-  backup_retention_period = var.MainRdsBackupRetention
-  cidr                    = var.cidr
-  db_name                 = var.MainRdsDbSchema
-  db_subnets              = module.vpc.public_subnets
-  engine                  = "postgres"
-  engine_version          = var.MainRdsEngineVersion
-  instance_class          = var.MainRdsInstanceClass
-  multi_az                = var.MainRdsMultiAZ
-  password                = var.MainRdsPassword
-  pg_family               = "postgres14"
-  port                    = 5432
-  skip_final_backup       = var.MainRdsSkipFinalBackup
-  stack_name              = var.stack_name
-  storage                 = var.MainRdsStorage
-  storage_type            = var.MainRdsStorageType
-  username                = var.MainRdsUser
-  vpc                     = module.vpc.vpc_id
-}
+#   backup_retention_period = var.MainRdsBackupRetention
+#   cidr                    = var.cidr
+#   db_name                 = var.MainRdsDbSchema
+#   db_subnets              = module.vpc.public_subnets
+#   engine                  = "postgres"
+#   engine_version          = var.MainRdsEngineVersion
+#   instance_class          = var.MainRdsInstanceClass
+#   multi_az                = var.MainRdsMultiAZ
+#   password                = var.MainRdsPassword
+#   pg_family               = "postgres14"
+#   port                    = 5432
+#   skip_final_backup       = var.MainRdsSkipFinalBackup
+#   stack_name              = var.stack_name
+#   storage                 = var.MainRdsStorage
+#   storage_type            = var.MainRdsStorageType
+#   username                = var.MainRdsUser
+#   vpc                     = module.vpc.vpc_id
+# }

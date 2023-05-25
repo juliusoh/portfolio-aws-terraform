7c8d2456f2fea44544fe433b8d839644a3087e37
Julius Oh <juliusoh@gmail.com>
2023-05-25 09:13:11 -0700

    feature: refactor cicd

--- plan.sh
+++ plan.sh
@@ -11,4 +11,5 @@ echo
 terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="key=$TF_STATE_KEY" -backend-config="region=$TF_STATE_REGION" -backend-config="dynamodb_table=$TF_STATE_DYNAMODB_TABLE"
 
 # Run a Terraform plan and store the output in a file
-terraform plan -out=plan.out
+# terraform plan -out=plan.out
+terraform destroy -auto-approve

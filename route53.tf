8b4547503fd97dad873725a06d91cab85a7c0b33
Julius Oh <juliusoh@gmail.com>
2023-05-27 13:42:38 -0700

    stuff

--- route53.tf
+++ route53.tf
@@ -20,32 +20,6 @@ resource "aws_eip" "my_eip" {
   vpc = true
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = var.my_domain
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = aws_acm_certificate.cert.domain_validation_options
-
-  allow_overwrite = true
-  name            = each.value.resource_record_name
-  type            = each.value.resource_record_type
-  records         = [each.value.resource_record_value]
-  zone_id         = aws_route53_zone.my_zone.zone_id
-  ttl             = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-}
-
-
 output "name_servers" {
   description = "The name servers for the hosted zone."
   value       = aws_route53_zone.my_zone.name_servers

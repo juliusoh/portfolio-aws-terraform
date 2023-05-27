ce4cbf5abf532ab4415b72c96b54446b1d59cc8a
Julius Oh <juliusoh@gmail.com>
2023-05-27 13:53:32 -0700

    aws load bvalancer

--- tf_aws_eks/helm.tf
+++ tf_aws_eks/helm.tf
@@ -3,7 +3,7 @@ provider "helm" {
       host                  = aws_eks_cluster.eks-cluster.endpoint
       cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority.0.data)
       exec {
-        api_version = "client.authentication.k8s.io/v1alpha1"
+        api_version = "client.authentication.k8s.io/v1beta1"
         command     = "aws"
         args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks-cluster.id]
       }

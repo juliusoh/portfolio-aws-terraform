74bebdc4e3e2978550019a11dde7a11d3bc2b6c8
Julius Oh <juliusoh@gmail.com>
2023-05-27 11:49:54 -0700

    test

--- tf_aws_vpc/main.tf
+++ tf_aws_vpc/main.tf
@@ -1,120 +1,89 @@
-data "aws_availability_zones" "available" {
-}
-
-resource "aws_vpc" "mod" {
-  cidr_block           = var.cidr
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support   = var.enable_dns_support
-
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
-    Name = var.stack_name
+    Name = "${var.name}"
   }
 }
 
-resource "aws_internet_gateway" "mod" {
-  vpc_id = aws_vpc.mod.id
+resource "aws_subnet" "subnets_public" {
+  depends_on = [aws_vpc.vpc]
+
+  for_each = {
+    for i, v in var.subnets : i => v
+
+    if v.type == "public"
+  }
+  cidr_block = each.value.cidr_block
+  tags = {
+    Name = each.value.name
+  }
+  availability_zone       = join("", [var.region, each.value.az])
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnets_private" {
+  depends_on = [aws_vpc.vpc]
+  for_each = {
+    for i, v in var.subnets : i => v
+
+    if v.type == "private"
+  }
+  cidr_block = each.value.cidr_block
+  tags = {
+    Name = each.value.name
+  }
+  availability_zone = join("", [var.region, each.value.az])
+  vpc_id            = aws_vpc.vpc.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  depends_on = [aws_vpc.vpc]
+  vpc_id     = aws_vpc.vpc.id
 
   tags = {
-    Name = "${var.stack_name}-igw"
+    Name = "${var.name}"
   }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id           = aws_vpc.mod.id
-  propagating_vgws = var.public_propagating_vgws
+resource "aws_route_table" "rt_public" {
+  depends_on = [aws_vpc.vpc]
+  vpc_id     = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name}-public"
+  }
+}
+
+resource "aws_route_table" "rt_private" {
+  depends_on = [aws_vpc.vpc]
+  vpc_id     = aws_vpc.vpc.id
 
   tags = {
-    Name = "${var.stack_name}-rt-public"
+    Name = "${var.name}-private"
   }
 }
 
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.rt_public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.mod.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = element(aws_route_table.private.*.id, count.index)
+resource "aws_route" "nat_rt_entry" {
+  route_table_id         = aws_route_table.rt_private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.natgw.*.id, count.index)
-  count                  = length(var.private_subnets)
+  nat_gateway_id         = var.nat_gateway_id
 }
 
-resource "aws_route_table" "private" {
-  vpc_id           = aws_vpc.mod.id
-  propagating_vgws = var.private_propagating_vgws
-  count            = length(var.private_subnets)
-
-  tags = {
-    Name = "${var.stack_name}-rt-private-${aws_route_table.private[count.index].id}"
-  }
+resource "aws_route_table_association" "rt_private_association" {
+  for_each       = { for i, v in aws_subnet.subnets_private : i => v }
+  subnet_id      = aws_subnet.subnets_private[each.key].id
+  route_table_id = aws_route_table.rt_private.id
 }
-
-
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
-
-  vpc_id            = aws_vpc.mod.id
-  cidr_block        = element(concat(var.private_subnets), count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
-  tags = {
-    Name = "${var.stack_name}-subnet-private-${element(data.aws_availability_zones.available.names, count.index)}"
-  }
-}
-resource "aws_subnet" "eks" {
-  count = length(var.eks_subnets)
-
-  vpc_id            = aws_vpc.mod.id
-  cidr_block        = element(concat(var.eks_subnets), count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.stack_name}-subnet-eks-${element(data.aws_availability_zones.available.names, count.index)}"
-  }
-}
-
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
-
-  vpc_id            = aws_vpc.mod.id
-  cidr_block        = element(concat(var.public_subnets), count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
-  tags = {
-    Name = "${var.stack_name}-subnet-public-${element(data.aws_availability_zones.available.names, count.index)}"
-  }
-
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-}
-
-resource "aws_eip" "nateip" {
-  vpc   = true
-  count = length(var.private_subnets)
-}
-
-resource "aws_nat_gateway" "natgw" {
-  allocation_id = element(aws_eip.nateip.*.id, count.index)
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  count         = length(var.private_subnets)
-
-  depends_on = [aws_internet_gateway.mod]
-}
-
-resource "aws_route_table_association" "private" {
-  count          = length(var.private_subnets)
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
-}
-resource "aws_route_table_association" "eks" {
-  count          = length(var.eks_subnets)
-  subnet_id      = element(aws_subnet.eks.*.id, count.index)
-  route_table_id = element(aws_route_table.public.*.id, count.index)
-}
-
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnets)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "rt_public_association" {
+  for_each       = { for i, v in aws_subnet.subnets_public : i => v }
+  subnet_id      = aws_subnet.subnets_public[each.key].id
+  route_table_id = aws_route_table.rt_public.id
 }

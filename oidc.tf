f816965ee9448d1ea6b6fb60fba58dc5ef33d03b
Julius Oh <juliusoh@gmail.com>
2023-05-27 12:16:06 -0700

    test

--- oidc.tf
+++ oidc.tf
@@ -1,9 +1,9 @@
-module "github_actions_oidc" {
-  source = "./tf_aws_oidc"  // path to the module
+# module "github_actions_oidc" {
+#   source = "./tf_aws_oidc"  // path to the module
 
-  github_url      = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
-  oidc_url        = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  org_repo        = "juliusoh/portfolio-aws-terraform"
-  allowed_actions = ["*"]
-}
+#   github_url      = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+#   oidc_url        = "https://token.actions.githubusercontent.com"
+#   client_id_list  = ["sts.amazonaws.com"]
+#   org_repo        = "juliusoh/portfolio-aws-terraform"
+#   allowed_actions = ["*"]
+# }

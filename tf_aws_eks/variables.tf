bfe331ef239ed048c5911ff87115bb600f8f5017
Julius Oh <juliusoh@gmail.com>
2023-06-04 16:59:28 -0700

    argocd

--- tf_aws_eks/variables.tf
+++ tf_aws_eks/variables.tf
@@ -2,6 +2,12 @@ variable "instance_types" {
   type = list(string)
 }
 
+variable "deploy_argocd" {
+  description = "Control the deployment of argocd"
+  type        = bool
+  default     = false
+}
+
 variable "region" {}
 
 variable "stack_name" {}

64baad8cc2fedb88f2c9791b6b8090dc0cc2eb9c
Julius Oh <juliusoh@gmail.com>
2023-06-04 17:04:32 -0700

    argocd

--- tf_aws_eks/argocd.tf
+++ tf_aws_eks/argocd.tf
@@ -1,11 +1,12 @@
 resource "helm_release" "argocd" {
-    name = "argocd"
-    count = var.deploy_argocd ? 1 : 0
-    repository = "https://argoproj.github.io/argo-helm"
-    chart = "argo-cd"
-    namespace = "argocd"
-    create_namespace = true 
-    version = "3.35.4"
+  count = var.deploy_argocd ? 1 : 0
 
-    values=[file("values/argocd.yaml")]
+  name = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true 
+  version = "3.35.4"
+
+  values = [file("${path.module}/values/argocd.yaml")]
 }

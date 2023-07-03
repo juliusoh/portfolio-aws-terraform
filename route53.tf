7b2ff8c6c35593c7da206886899748843b94fb78
Julius Oh <juliusoh@gmail.com>
2023-07-02 20:26:43 -0700

    add ingyuoh

--- route53.tf
+++ route53.tf
@@ -1,3 +1,15 @@
+variable "my_domain" {
+  description = "Your domain name"
+  type        = string
+  default     = "juliusoh.com" // replace with your domain
+}
+
+variable "ingyuoh" {
+  description = "Your domain name"
+  type        = string
+  default     = "ingyuoh.com" // replace with your domain
+}
+
 resource "aws_route53_zone" "my_zone" {
   name = var.my_domain
 }
@@ -13,11 +25,33 @@ resource "aws_route53_record" "www" {
   }
 }
 
-variable "my_domain" {
-  description = "Your domain name"
-  type        = string
-  default     = "juliusoh.com" // replace with your domain
+# IngyuOh
+
+resource "aws_route53_zone" "ingyuoh" {
+  name = var.ingyuoh
 }
+
+resource "aws_route53_record" "ingyuoh" {
+  zone_id = aws_route53_zone.ingyuoh.zone_id
+  name    = "${var.ingyuoh}"
+  type    = "A"
+  alias {
+    evaluate_target_health = false 
+    zone_id = "Z368ELLRRE2KJ0"
+    name   = "dualstack.k8s-sharedlb-0b6b1133ba-867358731.us-west-1.elb.amazonaws.com"
+  }
+}
+resource "aws_route53_record" "ingyuoh" {
+  zone_id = aws_route53_zone.ingyuoh.zone_id
+  name    = "www.${var.ingyuoh}"
+  type    = "A"
+  alias {
+    evaluate_target_health = false 
+    zone_id = "Z368ELLRRE2KJ0"
+    name   = "dualstack.k8s-sharedlb-0b6b1133ba-867358731.us-west-1.elb.amazonaws.com"
+  }
+}
+
 
 output "name_servers" {
   description = "The name servers for the hosted zone."

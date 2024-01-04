10188bc86069f976fcfb21fc338d514cfb85137e
Julius Oh <juliusoh@gmail.com>
2024-01-03 18:52:23 -0800

    increase size

--- metrics-server.tf
+++ metrics-server.tf
@@ -1,28 +1,27 @@
-data "aws_eks_cluster" "eks" {
-  name       = module.eks.eks_cluster_name
-  depends_on = [module.eks]
-}
+# data "aws_eks_cluster" "eks" {
+#   name       = module.eks.eks_cluster_name
+#   depends_on = [module.eks]
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name       = module.eks.eks_cluster_name
-  depends_on = [module.eks]
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name       = module.eks.eks_cluster_name
+#   depends_on = [module.eks]
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    config_path = "~/.kube/config"
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-resource "helm_release" "metrics_server" {
-  name = "metrics-server"
+# resource "helm_release" "metrics_server" {
+#   name = "metrics-server"
 
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-  chart      = "metrics-server"
-  namespace  = "kube-system"
-  version    = "3.11.0"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
+#   chart      = "metrics-server"
+#   namespace  = "kube-system"
+#   version    = "3.11.0"
     
-}
+# }

a1b9ac178a2e6985bd2585ef56cf228c2a53ec8c
Julius Oh <juliusoh@gmail.com>
2024-01-03 19:23:27 -0800

    increase size

--- tf_aws_eks/variables.tf
+++ tf_aws_eks/variables.tf
@@ -16,16 +16,3 @@ variable "subnet_ids" {}
 
 variable "account_id" {}
 
-variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
-
-  default = [
-    {
-      name    = "aws-ebs-csi-driver"
-      version = "v1.26.0"
-    }
-  ]
-}

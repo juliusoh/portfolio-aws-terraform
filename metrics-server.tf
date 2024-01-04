a0eff1b9b39e3fa8f4af874b2cc2eb8090eb027f
Julius Oh <juliusoh@gmail.com>
2024-01-03 18:50:25 -0800

    increase size

--- metrics-server.tf
+++ metrics-server.tf
@@ -13,15 +13,16 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
+    config_path = "~/.kube/config"
   }
 }
 
-# resource "helm_release" "metrics_server" {
-#   name = "metrics-server"
+resource "helm_release" "metrics_server" {
+  name = "metrics-server"
 
-#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
-#   chart      = "metrics-server"
-#   namespace  = "kube-system"
-#   version    = "3.11.0"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.11.0"
     
-# }
+}

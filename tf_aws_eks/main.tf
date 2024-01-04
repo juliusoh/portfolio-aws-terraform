280a8640ec92b95e346dae15d7df33bcbdc3de5f
Julius Oh <juliusoh@gmail.com>
2024-01-03 18:44:36 -0800

    increase size

--- tf_aws_eks/main.tf
+++ tf_aws_eks/main.tf
@@ -19,8 +19,8 @@ resource "aws_eks_node_group" "eks-node-group" {
   node_role_arn   = aws_iam_role.eks-node-group.arn
   subnet_ids      = var.subnet_ids
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 

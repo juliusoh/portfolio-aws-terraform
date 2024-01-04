18df061f3b06e9c004e749e72123a581d6b34db0
Julius Oh <juliusoh@gmail.com>
2024-01-03 18:47:21 -0800

    increase size

--- metrics-server.tf
+++ metrics-server.tf
@@ -16,12 +16,12 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "metrics_server" {
-  name = "metrics-server"
+# resource "helm_release" "metrics_server" {
+#   name = "metrics-server"
 
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-  chart      = "metrics-server"
-  namespace  = "kube-system"
-  version    = "3.11.0"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
+#   chart      = "metrics-server"
+#   namespace  = "kube-system"
+#   version    = "3.11.0"
     
-}
+# }

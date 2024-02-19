009b95394ddbd399a86c0258c3ce177fc27549a1
Julius Oh <juliusoh@gmail.com>
2024-02-19 11:07:47 -0800

    karpenter

--- tf_aws_eks/karpenter.tf
+++ tf_aws_eks/karpenter.tf
@@ -26,7 +26,7 @@ resource "aws_iam_policy" "karpenter_controller" {
   name   = "KarpenterController"
 }
 
-resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach_karpenter" {
   role       = aws_iam_role.karpenter_controller.name
   policy_arn = aws_iam_policy.karpenter_controller.arn
 }

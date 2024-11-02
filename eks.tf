f6009d094a211b67e96ac0369bc0edb2345f6c8d
Julius Oh <juliusoh@gmail.com>
2024-11-02 12:21:05 -0700

    create eks istio cluster

--- eks.tf
+++ eks.tf
@@ -8,3 +8,14 @@ module "eks" {
   account_id     = data.aws_caller_identity.current.account_id
   capacity_type  = "SPOT"  # Add this line
 }
+
+module "istio_eks" {
+  source = "./tf_aws_eks"
+  deploy_argocd = false  # We don't need ArgoCD for the tutorial
+  instance_types = ["t3.large"]  # Single instance type is fine for testing
+  region         = var.region
+  stack_name     = "istio-tutorial"  # Different stack name to differentiate
+  subnet_ids     = module.vpc.eks_subnets
+  account_id     = data.aws_caller_identity.current.account_id
+  capacity_type  = "ON_DEMAND"  # Using on-demand for stability during tutorial
+}

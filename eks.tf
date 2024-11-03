817563179e6267c5cf9e8153fb26394c89c300f4
Julius Oh <juliusoh@gmail.com>
2024-11-03 13:54:54 -0800

    create eks istio cluster

--- eks.tf
+++ eks.tf
@@ -11,15 +11,15 @@ module "eks" {
   capacity_type  = "SPOT"  # Add this line
 }
 
-module "istio_eks" {
-  source = "./tf_aws_eks"
-  deploy_argocd = false
-  deploy_karpenter = false
-  deploy_lb_controller = false
-  instance_types = ["t3.large"]
-  region         = var.region
-  stack_name     = "istio-tutorial"
-  subnet_ids     = module.vpc.eks_subnets
-  account_id     = data.aws_caller_identity.current.account_id
-  capacity_type  = "ON_DEMAND"
-}
+# module "istio_eks" {
+#   source = "./tf_aws_eks"
+#   deploy_argocd = false
+#   deploy_karpenter = false
+#   deploy_lb_controller = false
+#   instance_types = ["t3.large"]
+#   region         = var.region
+#   stack_name     = "istio-tutorial"
+#   subnet_ids     = module.vpc.eks_subnets
+#   account_id     = data.aws_caller_identity.current.account_id
+#   capacity_type  = "ON_DEMAND"
+# }

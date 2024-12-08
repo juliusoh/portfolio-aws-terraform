9d8224f6e4e585ba76fd9ca1b7f7d91e43340ff0
Julius Oh <julius.oh@xevant.com>
2024-12-07 18:18:05 -0800

    fix: iam

--- tf_aws_eks/iam.tf
+++ tf_aws_eks/iam.tf
@@ -44,29 +44,6 @@ resource "aws_iam_role" "eks-node-group" {
 POLICY
 }
 
-resource "aws_iam_role" "lb_controller_role" {
-  name = "tf-${var.stack_name}-lb-controller-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-  	"Effect": "Allow",
-  	"Principal": {
-  		"Federated": "arn:aws:iam::${var.account_id}:oidc-provider/${aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer}"
-  	},
-  	"Action": "sts:AssumeRoleWithWebIdentity",
-  	"Condition": {
-  		"StringEquals": {
-  			"${aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer}:aud": "sts.amazonaws.com",
-  			"${aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
-  		}
-  	}
-  }]
-}
-POLICY
-}
-
 data "tls_certificate" "eks" {
     url = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
 }
@@ -97,8 +74,23 @@ data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy"
 
 resource "aws_iam_role" "aws_load_balancer_controller" {
   count = var.deploy_lb_controller ? 1 : 0
-  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
-  name               = "aws-load-balancer-controller-${var.stack_name}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller",
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud": "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+  name = "aws-load-balancer-controller-${var.stack_name}"
 }
 
 resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
